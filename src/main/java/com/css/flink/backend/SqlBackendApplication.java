a9da1970de9da8e3256b068a57ad256f447a64b0
JamesLiu <906028518@qq.com>
2021-02-03 10:24:38 +0800

    FLINK-18 [James] sample API created

--- src/main/java/com/css/flink/backend/SqlBackendApplication.java
+++ src/main/java/com/css/flink/backend/SqlBackendApplication.java
@@ -2,15 +2,25 @@ package com.css.flink.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * @author liuchenyu
  */
 @SpringBootApplication
+@RestController
 public class SqlBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SqlBackendApplication.class, args);
+    }
+
+    @GetMapping("test")
+    public Mono<String> test(){
+        return Mono.just("hello world");
     }
 
 }
